9e332f5fef42ea32eb365dafb36431c13bc8a64a
usersoket <badanpenyelidikmarket@gmail.com>
2026-06-16 16:12:18 +0700

    Update sky-merah.php

--- sky-merah.php
+++ sky-merah.php
@@ -716,7 +716,7 @@ body{background: black;}
 #loginbox td { border-radius:5px; font-size:11px; }
 </style>
 <title>~ ALFA TEaM Shell-v'.__ALFA_VERSION__.'-'.__ALFA_CODE_NAME__.' ~</title><center>
-<center><img style="border-radius:100px;" width="500" height="250" alt="alfa team 2012" draggable="false" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSqgbT5IDb_O75-cago6MLhA78G2ViKS8o0voIPSZ4OL-49aSV_-gSwUx9WOaUG7lQ5oDG5HOWHseQUs3CdC7DAxQsy1eye9O0hqrLGzA3WTF2ZMmJzd-diC1IMQAFBqRd01PVgpF3s87Js5EHu81EfPQLcAUvxnJFFFB3V3CIugsJuD0TFIgVoAM_t8gJ/s1200/logo-sky.jpg" /></center>
+<center><img style="border-radius:100px;" width="500" height="500" alt="alfa team 2012" draggable="false" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSqgbT5IDb_O75-cago6MLhA78G2ViKS8o0voIPSZ4OL-49aSV_-gSwUx9WOaUG7lQ5oDG5HOWHseQUs3CdC7DAxQsy1eye9O0hqrLGzA3WTF2ZMmJzd-diC1IMQAFBqRd01PVgpF3s87Js5EHu81EfPQLcAUvxnJFFFB3V3CIugsJuD0TFIgVoAM_t8gJ/s1200/logo-sky.jpg" /></center>
 <div id=loginbox><p><font face="verdana,arial" size=-1>
 <center><table cellpadding=\'2\' cellspacing=\'0\' border=\'0\' id=\'ap_table\'>
 <tr><td bgcolor="green"><table cellpadding=\'0\' cellspacing=\'0\' border=\'0\' width=\'100%\'><tr><td bgcolor="green" align=center style="padding:2;padding-bottom:4"><b><font color="white" size=-1 color="white" face="verdana,arial"><b>~ ALFA TEaM Shell-v'.__ALFA_VERSION__.'-'.__ALFA_CODE_NAME__.' ~</b></font></th></tr>
@@ -1292,7 +1292,7 @@ echo '<input type="hidden" name="charset">
 <tr>
 <td><span class="header_vars">User:</span></td>
 <td><span class="header_values" id="header_userid">'. $uid . ' [ ' . $user . ' ] </span><span class="header_vars"> Group: </span><span class="header_values" id="header_groupid">' . $gid . ' [ ' . $group . ' ]</span> </td>
-<td width="12%" rowspan="8"><img style="border-radius:100px;" width="300" height="170" alt="alfa team 2012" draggable="false" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSqgbT5IDb_O75-cago6MLhA78G2ViKS8o0voIPSZ4OL-49aSV_-gSwUx9WOaUG7lQ5oDG5HOWHseQUs3CdC7DAxQsy1eye9O0hqrLGzA3WTF2ZMmJzd-diC1IMQAFBqRd01PVgpF3s87Js5EHu81EfPQLcAUvxnJFFFB3V3CIugsJuD0TFIgVoAM_t8gJ/s1200/logo-sky.jpg" /></td>
+<td width="12%" rowspan="8"><img style="border-radius:100px;" width="170" height="170" alt="alfa team 2012" draggable="false" src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEgSqgbT5IDb_O75-cago6MLhA78G2ViKS8o0voIPSZ4OL-49aSV_-gSwUx9WOaUG7lQ5oDG5HOWHseQUs3CdC7DAxQsy1eye9O0hqrLGzA3WTF2ZMmJzd-diC1IMQAFBqRd01PVgpF3s87Js5EHu81EfPQLcAUvxnJFFFB3V3CIugsJuD0TFIgVoAM_t8gJ/s1200/logo-sky.jpg" /></td>
 </tr>
 <tr>
 <td><span class="header_vars">PHP:</span></td>
